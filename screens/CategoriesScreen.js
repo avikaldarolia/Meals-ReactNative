58c5b9e589a780a8a00f05dee0f0218ab674872a
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-15 14:12:49 +0530

    Navigation between 2 pages

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,11 +2,14 @@ import { FlatList, ScrollView, StyleSheet, View } from 'react-native'
 import CategoryGridTile from '../components/CategoryGridTile';
 import { CATEGORIES } from "../data/dummy-data"
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-}
+function CategoriesScreen({ navigation }) {
 
-function CategoriesScreen() {
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate("MealsOverview")
+        }
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />
+    }
     return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2} />
 }
 
