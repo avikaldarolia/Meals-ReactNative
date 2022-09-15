6ee66e6ac984a5ed3c1463b306431f0b4c7dd7a3
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-15 16:08:13 +0530

    useLayoutEffect hook to set title of the page as categoryTitle

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,13 @@
+import { useLayoutEffect } from "react";
 import { StyleSheet, FlatList, View } from "react-native"
 import MealItem from "../components/MealItem";
-import { MEALS } from "../data/dummy-data"
+import { MEALS, CATEGORIES } from "../data/dummy-data"
 function MealsOverviewScreen({ route, navigation }) {
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0;
     })
-
     function renderMealItem(itemData) {
         const item = itemData.item;
         const mealItemProps = {
@@ -17,8 +17,15 @@ function MealsOverviewScreen({ route, navigation }) {
             complexity: item.complexity,
             duration: item.duration,
         }
-        return <MealItem {...mealItemProps}  />
+        return <MealItem {...mealItemProps} />
     }
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title
+        navigation.setOptions({
+            title: categoryTitle
+        })
+    }, [catId, navigation])
 
     return (
         <View style={styles.container}>
